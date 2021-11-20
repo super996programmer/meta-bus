b38f1c80f4d7be4717276ee74c3b7bd35a32de74
Cheng Yi Lin <cheng.yi.lin.217@gmail.com>
2021-11-20 23:21:50 +0800

    feat : create component `BusKeyBoard`

---
+++ src/components/KeyBoard/index.tsx
@@ -0,0 +1,76 @@
+import { FC } from 'react'
+import styled from 'styled-components';
+import KeyButtonList, {IKeyButtonList} from './setting/keyButtonList';
+
+const Container = styled.div`
+    padding: 1rem;
+    background: #F9F9F9;
+`;
+
+const KeyBoardContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-template-rows: auto;
+    gap: 4px 7px;
+    max-width: 375px;
+    margin: 0 auto;
+`;
+
+const KeyButton = styled.div<{color?: string}>`
+    cursor: pointer;
+    background: #ffffff;
+    border-radius: 10px;
+    font-size: 20px;
+    padding: 8px;
+    text-align: center;
+    color: ${({color}) => color || '#000' };
+`;
+
+interface IBusKeyBoard {
+    value: string;
+    setKeyValue: (key: string) => void;
+}
+
+const BusKeyBoard: FC<IBusKeyBoard> = (props) => {
+    const { value, setKeyValue } = props;
+
+    const handleClickKey = (keySetting: IKeyButtonList) => {
+        // eslint-disable-next-line no-console
+        console.log(value)
+
+        switch(keySetting.type){
+            case 'Line': {
+                setKeyValue(keySetting.value);
+                break;
+            }
+            case 'Text': {
+                const newValue = value + keySetting.value;
+                setKeyValue(newValue);
+                break;
+            }
+            case 'Clear': {
+                setKeyValue('');
+                break;
+            }
+            default:
+                setKeyValue(keySetting.value)
+        }
+    }
+
+    return (
+        <Container>
+            <KeyBoardContainer>
+                {KeyButtonList.map(keySetting => (
+                    <KeyButton
+                        key={`key-button-${keySetting.value}`}
+                        color={keySetting.color}
+                        onClick={() => handleClickKey(keySetting)}>
+                        {keySetting.value}
+                    </KeyButton>
+                ))}
+            </KeyBoardContainer>
+        </Container>
+    )
+}
+
+export default BusKeyBoard;
